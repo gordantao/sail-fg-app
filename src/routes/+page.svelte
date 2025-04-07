3388b757af0db64ac67ed70d1859ee7dd58a5241
GORDAN TAO <taogordan@gmail.com>
2025-04-07 17:07:48 -0400

    Updated style to carolina.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
   let yardLine = 35;
-  let hash = "center";
   let windSpeed = 0;
-  let windDirection = "none";
   let probability: number = 0.0;
 
   async function predictProbability() {
@@ -11,7 +9,7 @@
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ yardLine, hash, windSpeed, windDirection }),
+      body: JSON.stringify({ yardLine, windSpeed }),
     });
 
     const data = await response.json();
@@ -37,32 +35,13 @@
 
   <div class="space-y-2">
     <label>Yard Line:</label>
-    <input type="range" min="10" max="80" bind:value={yardLine} />
+    <input type="range" min="17" max="90" bind:value={yardLine} />
     <span>{yardLine} yard line</span>
-  </div>
-
-  <div class="space-y-2">
-    <label>Hash Mark:</label>
-    <select bind:value={hash}>
-      <option value="left">Left</option>
-      <option value="center">Center</option>
-      <option value="right">Right</option>
-    </select>
   </div>
 
   <div class="space-y-2">
     <label>Wind Speed (mph):</label>
     <input type="number" bind:value={windSpeed} min="0" max="50" />
-  </div>
-
-  <div class="space-y-2">
-    <label>Wind Direction:</label>
-    <select bind:value={windDirection}>
-      <option value="none">None</option>
-      <option value="headwind">Headwind</option>
-      <option value="tailwind">Tailwind</option>
-      <option value="crosswind">Crosswind</option>
-    </select>
   </div>
 
   <button
@@ -86,14 +65,16 @@
   >
     <!-- Horizontal Endzones with matching stripe width and vertical text -->
     <div
-      class="absolute inset-y-0 left-0 w-[10%] bg-blue-900 flex flex-col items-center justify-center text-white text-xs"
+      class="absolute inset-y-0 left-0 w-[10%] font-black flex flex-col items-center justify-center text-white text-xs"
+      style="background-color: #56a0d3"
     >
-      <span class="transform rotate-270">ENDZONE</span>
+      <span class="transform rotate-270">CAROLINA</span>
     </div>
     <div
-      class="absolute inset-y-0 right-0 w-[10%] bg-red-900 flex flex-col items-center justify-center text-white text-xs"
+      class="absolute inset-y-0 right-0 w-[10%] font-black flex flex-col items-center justify-center text-white text-xs"
+      style="background-color: #56a0d3"
     >
-      <span class="transform rotate-90">ENDZONE</span>
+      <span class="transform rotate-90">CAROLINA</span>
     </div>
 
     <!-- Goalpost (left side only) -->
@@ -121,16 +102,18 @@
 
     <!-- Midfield Logo -->
     <div
-      class="absolute top-1/2 left-[50%] w-12 h-12 bg-white/20 rounded-full transform -translate-x-1/2 -translate-y-1/2 text-xs flex items-center justify-center text-white"
+      class="absolute top-1/2 left-[50%] w-12 h-12 transform -translate-x-1/2 -translate-y-1/2 text-xs flex items-center justify-center text-white"
     >
-      LOGO
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/North_Carolina_Tar_Heels_logo.svg/2560px-North_Carolina_Tar_Heels_logo.svg.png"
+      />
     </div>
 
     <!-- Kicker Marker -->
     <div
       class="absolute w-2 h-2 bg-yellow-300 rounded-full shadow-md border border-white"
       style="
-        top: {hashToPercent(hash)};
+        top: 50%;
         transform: translateY(-50%);
         left: calc(({yardLine} / 100) * 80% + 10%);
       "
